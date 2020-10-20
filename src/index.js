fb8a5a02a7cde9aedd41e09ecd9025dd52c59087
turowskaja@gmail.com <turowskaja@gmail.com>
2020-10-20 16:47:48 +0300

    init: start tsk towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) return [];
+
+  let result = [];
+
+  for(i = 0; i < matrix.length; i++){
+
+    if(i%2 != 0){
+      matrix[i].reverse()
+    }
+
+    for(j = 0; j < matrix[i].length; j++){
+      result.push(matrix[i][j]);
+    }
+
+  }
+  return result;
 }
